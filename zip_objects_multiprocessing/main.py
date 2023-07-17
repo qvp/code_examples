e936156323f6df64612e6cc246bc669dc3089fa4
qvp <bafojo@gmail.com>
2023-07-17 12:49:09 +0300

    фикс закрытия пула процессов zip_objects_multiprocessing

--- zip_objects_multiprocessing/main.py
+++ zip_objects_multiprocessing/main.py
@@ -56,43 +56,43 @@ def unpack_obj_archives_to_csv(id_level_file_path: Path | str, id_object_name_fi
         csv_id_level_writer = csv.writer(csv_id_level, delimiter=CSV_DELIMITER)
         csv_id_object_name_writer = csv.writer(id_object_name, delimiter=CSV_DELIMITER)
 
-        pool = Pool(processes=processes_count)
-        obj_queue = Manager().Queue()
+        with Pool(processes=processes_count) as pool:
+            obj_queue = Manager().Queue()
 
-        archives = list(dir_zip_files(archives_dir))
-        archives_count = len(archives)
-        for archive_patch in archives:
-            pool.apply_async(read_archive_objects, (obj_queue, archive_patch))
+            archives = list(dir_zip_files(archives_dir))
+            archives_count = len(archives)
+            for archive_patch in archives:
+                pool.apply_async(read_archive_objects, (obj_queue, archive_patch))
 
-        archives_processed = 0
-        while archives_processed < archives_count:
-            queue_msg: QueueMessage = obj_queue.get()
+            archives_processed = 0
+            while archives_processed < archives_count:
+                queue_msg: QueueMessage = obj_queue.get()
 
-            # Если получен новый объект - обрабатываем его
-            if queue_msg.type == QueueMessageType.NEW_OBJ:
-                obj: Obj = queue_msg.payload
-                csv_id_level_writer.writerow([obj.vars['id'], obj.vars['level']])
-                csv_id_object_name_writer.writerows([(obj.vars['id'], o) for o in obj.objects])
+                # Если получен новый объект - обрабатываем его
+                if queue_msg.type == QueueMessageType.NEW_OBJ:
+                    obj: Obj = queue_msg.payload
+                    csv_id_level_writer.writerow([obj.vars['id'], obj.vars['level']])
+                    csv_id_object_name_writer.writerows([(obj.vars['id'], o) for o in obj.objects])
 
-            # Если архив целиком обработан
-            # счетчик обработанных архивов увеличивается на 1
-            if queue_msg.type == QueueMessageType.ARCHIVE_PROCESSED:
-                archives_processed += 1
+                # Если архив целиком обработан
+                # счетчик обработанных архивов увеличивается на 1
+                if queue_msg.type == QueueMessageType.ARCHIVE_PROCESSED:
+                    archives_processed += 1
 
-            # Если при обработке одного из архивов произошла ошибка
-            # останавливаем обработку или продолжаем с другими архивами в зависимости от skip_errors
-            # счетчик обработанных архивов увеличивается на 1
-            if queue_msg.type == QueueMessageType.ERROR:
-                archives_processed += 1
-                if not skip_errors:
-                    pool.terminate()
-                    print('Process of archives terminated after error')
-                    exit(1)
+                # Если при обработке одного из архивов произошла ошибка
+                # останавливаем обработку или продолжаем с другими архивами в зависимости от skip_errors
+                # счетчик обработанных архивов увеличивается на 1
+                if queue_msg.type == QueueMessageType.ERROR:
+                    archives_processed += 1
+                    if not skip_errors:
+                        pool.terminate()
+                        print('Process of archives terminated after error')
+                        exit(1)
 
-        # Все архивы обработаны
-        work_seconds = round(timer() - start_time, 3)
-        print(f'{archives_count} archives processed in {work_seconds} seconds '
-              f'by {processes_count} pool processes')
+            # Все архивы обработаны
+            work_seconds = round(timer() - start_time, 3)
+            print(f'{archives_count} archives processed in {work_seconds} seconds '
+                  f'by {processes_count} pool processes')
 
 
 if __name__ == '__main__':
